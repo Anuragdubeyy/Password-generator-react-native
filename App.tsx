8ef39f712c1559e5e9678b0a198db6a116d3acbb
AnuragDubey67 <101169657+AnuragDubey67@users.noreply.github.com>
2024-08-03 16:10:50 +0530

    passwordSchema and define  useState

--- App.tsx
+++ App.tsx
@@ -1,12 +1,29 @@
 import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
 //for validation
 import * as Yup from 'yup';
 
 const PasswordSchema = Yup.object().shape({
-
-})
+  passwordLength: Yup.number()
+    .min(4, 'Should be min of 4 characters')
+    .max(16, 'Should be max of 16 characters')
+    .required('Length is required'),
+});
 export default function App() {
+  const [password, setPassword] = useState('');
+  const [isPasswordGenerated, setIsPasswordGenerated] = useState(false);
+  const [lowerCase, setLowerCase] = useState(true);
+  const [upperCase, setUpperCase] = useState(false);
+  const [namubers, useNumbers] = useState(false);
+  const [symbols, useSymbols] = useState(false);
+
+  const generatePasswordString = (passwordLength: number) => {
+    //
+  };
+  const createPassword = (characters: string, passwordLength: number) => {
+    //
+  };
+
   return (
     <View>
       <Text>App</Text>
